9fb0efb6052510027bcffcc5a7feb1449f017adb
Dim <dimbac@gamer.bo>
2019-02-16 17:51:11 +0700

    testing import export

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,10 @@
 // Global app controller
-import num from './test';
 
-const x = 23;
-console.log(`I imported ${num} from another module which test.js! Variable x is ${x}`);
+/* import string from './models/Search';
+//import{add as a, multiply as m, ID} from './views/searchView';
+import * as searchView from './views/searchView';
+
+//console.log(`Using imported functions! ${a(ID, 50)} and ${m(3, 5)}. ${string}`);
+console.log(`Using imported functions! ${searchView.add(searchView.ID, 50)} and ${searchView.multiply(3, 5)}. ${string}`); */
+
+
